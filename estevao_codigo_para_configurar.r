996f383ecd00ec35c2b9424ac30168a41c457a54
estevao87 <estevaobraz@gmail.com>
2024-01-28 18:51:54 -0300

    cria .Rproj e .gitignore

---
+++ estevao_codigo_para_configurar.r
@@ -0,0 +1,8 @@
+usethis::use_git_config(
+  user.name = "estevao87",
+  
+  user.email = "estevaobraz@gmail.com"
+)
+# usethis::create_github_token()
+# usethis::edit_r_environ()
+# usethis::git_sitrep()
